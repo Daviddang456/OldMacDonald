633e405f5fcf880150d15bd91bf09ab025eadab0
Daviddang456 <42555179+Daviddang456@users.noreply.github.com>
2018-11-12 11:07:44 -0800

    Finished program

--- Chick.java
+++ Chick.java
@@ -1,4 +1,34 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+  private String mySound;
+  private String myType;
+  private int Counter;
+  
+  public Chick(String type, String sound){
+     myType = type;
+     mySound = sound; 
+  }
+  
+  public Chick(String type, String sound, String sound1){
+    Counter = (int)(Math.random() * 100) + 1;
+    
+    if(Counter <= 50){
+      myType = type;
+      mySound = sound;
+      
+    }else if(Counter > 50){
+      myType = type;
+      mySound = sound1;
+    }
+    
+ }
+  
+  public String getSound(){
+    return mySound;
+  }
+  
+  public String getType(){
+     return myType;  
+  }
+  
 }
--- Farm.java
+++ Farm.java
@@ -1,4 +1,19 @@
 class Farm 
 {     
-   //your code here
-}
+   private int numAnimals = 3;
+   private Animal[] FarmAnimals = new Animal[numAnimals];
+   
+   public Farm(){
+    FarmAnimals[0] = new NamedCow("Cow", "Elise", "moo!");
+    FarmAnimals[1] = new Chick("Chick", "cluck!", "click!");
+    FarmAnimals[2] = new Pig("Pig", "oink!");
+   }
+   
+   public void animalSounds(){
+    for(int i = 0; i < FarmAnimals.length; i++){
+      System.out.println(FarmAnimals[i].getType() + " goes " + FarmAnimals[i].getSound());
+    }
+    System.out.println("By the way, the cow's name is " + ((NamedCow)FarmAnimals[0]).getName() + "!");
+ }
+ 
+} 
